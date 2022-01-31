15daa98ee99f1de49da97daebe7eda27eafe63bb
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-31 16:12:33 -0300

    feat: add Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import './App.css'
+import { Button } from './components/Button'
+
+function handleClick(name: string, age: number) {
+  console.log(`Me chama ${name} e tenho ${age} anos`)
+}
 
 function App() {
-  return <div className="App">Hello World</div>
+  return (
+    <div className="App">
+      {/* Utiliza os valores padrão */}
+      <Button onClick={() => handleClick('Leandro Lopes', 27)}>Button</Button>
+
+      {/* variant secondary */}
+      <Button
+        variant="secondary"
+        onClick={() => handleClick('Leandro Lopes', 27)}
+      >
+        Button
+      </Button>
+
+      {/* variant outline */}
+      <Button
+        variant="outline"
+        onClick={() => handleClick('Leandro Lopes', 27)}
+      >
+        Button
+      </Button>
+
+      {/* size small */}
+      <Button size="small" onClick={() => handleClick('Leandro Lopes', 27)}>
+        Button
+      </Button>
+
+      {/* size medium */}
+      <Button
+        variant="secondary"
+        size="medium"
+        onClick={() => handleClick('Leandro Lopes', 27)}
+      >
+        Button
+      </Button>
+
+      {/* full width */}
+      <Button fullWidth onClick={() => handleClick('Leandro Lopes', 27)}>
+        Button
+      </Button>
+    </div>
+  )
 }
 
 export default App
