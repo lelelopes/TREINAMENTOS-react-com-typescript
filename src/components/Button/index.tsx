6387cf8fcb864cd9fe631ffb69028fffc9128211
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-31 21:09:03 -0300

    feat: add Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,35 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import './Button.css'
+
+// informamos que o ButtonProps deve receber todas as propriedades
+// que um botão do React tem, mais as propriedades que nós estamos definindo
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'outline'
+  size?: 'small' | 'medium'
+  fullWidth?: boolean
+  onClick: () => void
+}
+
+export const Button = ({
+  children,
+  // aqui definimos o valor padrão das propriedades opcionais,
+  // se nada for passado para o componente esses valores serão utilizados
+  variant = 'primary',
+  size = 'medium',
+  fullWidth = false,
+  onClick,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={`button button-${variant} ${
+        variant === 'outline'
+      } button-${size} ${fullWidth ? 'button-fullWidth' : ''}`}
+      onClick={onClick}
+      {...props}
+    >
+      <span>{children}</span>
+    </button>
+  )
+}
